675b47c1c22c4cf48f67da8775944e5225016695
xelise-ehmedova-36 <ahmadovaxalisa@gmail.com>
2024-06-06 10:48:57 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// 1.Ekrana sehife açılandan sonra consola-a Hello world cixsin
+    // setTimeout(()=>{
+    //     console.log('Hello world');
+    // },5000)
+// 2.Counter = 0.her 5 saniyeden bir counter 1 vahid artsin,10olarsa dayansin
+// let Counter=0
+// const interval=setInterval=(()=>{
+// if(Counter===10){
+//     clearInterval(interval)
+//     console.log('vaxtiniz bitdi')
+// }
+//     console.log('Baslama muddeti: '+ Counter +' saniye');
+//     Counter++
+// },5000)
